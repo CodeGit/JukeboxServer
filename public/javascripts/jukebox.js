75670459aa9d08557fa3c9aced6978e32cf8fa4f
CodeGit <maq@ebi.ac.uk>
2015-07-06 14:55:33 +0100

    trying to fix issues with special characters in artist, album and playlists

--- public/javascripts/jukebox.js
+++ public/javascripts/jukebox.js
@@ -1,3 +1,12 @@
 /**
  * Created by maq on 06/07/2015.
  */
+
+"use strict";
+
+var Util = {};
+(function (context) {
+    context.processLink = function (baseUrl, component) {
+        console.log("processLink(" + baseUrl + ", " + component + ")");
+    };
+})(Util);
